fd5b0d9a73042b763255a7842612e5d06ea40d40
Cofo-admin <66292433+Cofo-admin@users.noreply.github.com>
2020-06-02 10:52:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo mkdir ~/.CoFo
-sudo chmod u=rx gen
-sudo chmod u=rx test
-sudo chmod u=rx temp
-sudo chmod u=rx main
+chmod a+x gen
+chmod a+x test
+chmod a+x temp
+chmod a+x main
 sudo cp gen /bin/LLgencofoLL
 sudo cp test /bin/LLtestcofoLL
 sudo cp temp /bin/LLtempcofoLL
